88402920b4e5313c5d8d0b88ee08f65e67f55d5e
Cloporte <anthonychaigneau@live.fr>
2024-03-12 12:59:42 +0100

    final tp3 + training

--- TD/ctd4.ml
+++ TD/ctd4.ml
@@ -14,11 +14,59 @@ let fact n =
   in aux 1 n 
 ;; 
 
+let rec reverse = function 
+  | [] -> []
+  | hd::tl -> (reverse tl) @ [hd]
+
 let reverse list =
-  let rec aux acc = function
+  let rec aux acc list = match list with
     | [] -> acc
     | hd::tl -> aux (hd::acc) tl
   in aux [] list
-;; 
+;;
 
+
+(* [1;2;3] *)
 (* EXERCICE 1 *)
+
+
+
+
+let rec occs = 
+  let rec inserer x = function
+    |[] -> [(x,1)]
+    |(y,n) :: l -> if x=y then (y,n+1) :: l
+        else (y,n) :: inserer x l
+  in function 
+    |[]->[]
+    |x :: l -> inserer x (occs l)
+;;
+
+let occs1 l = 
+  let rec insert e = function
+    | [] -> [(e,1)]
+    | (elem,occ)::tl -> 
+      if elem = e then (elem,occ+1)::tl
+      else (elem,occ)::(insert e tl)
+  in let rec occs_tail_r l_out = function
+    | [] -> l_out
+    | hd::tl -> occs_tail_r (insert hd l_out) tl
+  in occs_tail_r [] l
+;;
+
+let occs l = 
+  let rec insert e = function
+  | [] -> [(e,1)]
+  | (elem,occ)::tl -> 
+    if elem = e then (elem,occ+1)::tl
+    else (elem,occ)::(insert e tl)
+  in
+  List.fold_right (fun e acc -> insert e acc) l []
+;;
+
+let doublons l = 
+  List.fold_left 
+  (fun acc e -> 
+    if (List.exists (fun x -> x = e) acc) then acc 
+    else e::acc) 
+  [] l
--- TP/tp3.ml
+++ TP/tp3.ml
@@ -52,3 +52,46 @@ let rec f2 n = f2 (0 + n)
 let e1 = f1 5
 
 let e2 = f2 5
+
+
+let create n e = 
+  let rec aux acc e n = match n with
+    | 0 -> acc
+    | _ -> aux (e::acc) e (n-1)
+  in aux [] e n
+;; 
+
+let trie l =
+  match l with | [] -> true | x::y ->
+  let rec aux acc e = function
+    | [] -> acc
+    | hd::tl ->  aux (e < hd && acc) hd tl
+  in aux true x y
+;; 
+
+
+let cat l1 l2 =
+  let rec aux acc = function
+    | [] -> acc
+    | hd :: tl -> aux (hd :: acc) tl
+in aux l2 (aux [] l1)
+;;
+
+
+let insert x l =
+  let rec aux acc = function
+    | [] -> acc
+    | hd :: tl -> aux (hd :: acc) tl
+  in
+  let reverse l = aux [] l in
+  let cat l1 l2 = aux l2 (reverse l1) in
+  let rec insert_aux acc = function
+    | [] -> reverse (x :: acc)
+    | e :: tl ->
+        if e > x then cat (reverse acc) (x :: e :: tl)
+        else insert_aux (e :: acc) tl
+  in
+  insert_aux [] l
+;;
+
+let trier l = List.fold_left (fun acc e -> insert e acc) [] l;;
